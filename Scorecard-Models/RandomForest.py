d5f02a77423ac79b21f7ba9def3446437ad1ea60
jebarsonponraj07 <139750941+jebarsonponraj07@users.noreply.github.com>
2024-04-10 17:09:46 +0530

    Update RandomForest.py

--- Scorecard-Models/RandomForest.py
+++ Scorecard-Models/RandomForest.py
@@ -11,4 +11,5 @@ df = wd.get_args("df")
 
 clf = RandomForestClassifier(random_state=0).fit(df.drop(labels='capsule', axis=1).values, df['capsule'].values)
 col=df.drop(columns='capsule').columns.tolist()
-wd.create_model(clf,"sklearn","RandomForestClassifier",col,"df","capsule")
+wd.create_model(clf,"sklearn","RandomForestClassifier","df",col,"capsule")
+
